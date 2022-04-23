da66408ce103c18771ba2b061f42ca17132ad7c3
22savagexxx <staalaibekov2@mail.ru>
2022-04-23 18:18:06 +0600

    first commit

---
+++ problem1.py
@@ -0,0 +1,32 @@
+#a = ['qwerty', True, False, 545, 5.2]
+#print(type(a[2]))
+
+#NAMES=['JACK', 'JIMME', 'JACKSON', 'JHON', 'JACKSON', 'JHON', 'JACK', 'JIMMY', 'JACK', 'JACKSON', 'JHON', 'JACK', 'JIMMY', 'JACK', 'JACKSON', 'JHON']
+#print(NAMES.count('JACK'))
+
+#del NAMES[0::2]
+#print(NAMES)
+
+#fio = []
+#fio.append(input('Введите свое имя: '))
+#fio.append(input('Введите фамилию: '))
+#fio.append(input('Любимый язык программирования: '))
+#print(fio)
+
+#NAMES=['JACK', 'JIMME', 'JACKSON', 'JHON', 'JACKSON']
+#a = ('     ')
+#print(a.join(NAMES))
+
+
+#a = ('asd', 542, 5.1, True, False)
+#print(type(a[3]))
+
+#a = ('dsafds', 'etwqw', 'sfsqe')
+#print(a[0])
+#print(a[1])
+#print(a[2])
+
+
+
+#a = (1, 2, ('q', ('w', 'e'), 0), 3, (4, ('r', (7, ('x1'), 'y'), 't', 8), 5, 6))
+#print(a[4][1][1][1])
